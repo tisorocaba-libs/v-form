812109286ee5adb2e40ed78104c0554a1254e3ae
Victor Bastos <vbastos@sorocaba.sp.gov.br>
2017-05-24 18:28:43 -0300

    v-mask

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+import Vue from 'vue';
+import VueMask from 'v-mask';
 import register from './src/register';
 import input from './src/input';
 import select from './src/select';
 import textarea from './src/textarea';
+
+Vue.use(VueMask);
 
 export {
 	register,
